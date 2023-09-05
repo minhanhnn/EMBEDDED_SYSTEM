240accda0e523958b3dad44398740b477c4d9dbd
MinhAnh <minhanh.sp.146@gmail.com>
2023-09-05 08:35:58 +0700

    update main.c

--- PROJECT_W1/XC8Application1/XC8Application1/main.c
+++ PROJECT_W1/XC8Application1/XC8Application1/main.c
@@ -6,9 +6,9 @@ int main(void)
 	DDRB = 0x01; //Takes PORTB as output
     while(1)
     {
-		PORTB = 0x01; //Turns ON All LEDS
+		PORTB = 0x01; //Turns ON A LED
 		_delay_ms(1000); //1 second delay
-		PORTB = 0x00; //Turns OFF All LEDS
+		PORTB = 0x00; //Turns OFF A LED
 		_delay_ms(1000); //1 second delay
     }
 }
